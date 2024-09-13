90f6021a6f66e3845cbb76ee346656fa7f2d3131
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-09-13 12:42:17 -0400

    fixed some minor bugs

--- src/main/java/org/example/repository/OrderRepository.java
+++ src/main/java/org/example/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package org.example.repository;
 
 import org.example.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {}
 
